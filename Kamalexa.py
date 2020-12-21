573a455daa995405148bcc9feee40d42dcd68967
Kamalpreet-3223 <ksingh7_be20@thapar.edu>
2020-12-21 17:46:25 +0530

    Fixed a bug

--- Kamalexa.py
+++ Kamalexa.py
@@ -46,55 +46,63 @@ def takeCommand():
 if __name__ == "__main__":
     wishMe()
     while True:
-        query = takeCommand().lower()
-        if 'wikipedia' in query:
-            speak("Searching Wikipedia...")
-            query = query.replace("wikipedia" , "")
-            results = wikipedia.summary(query, sentences=2)
-            print(results)
-            speak("According to wikipedia")
-            speak(results)
+        try:
+            query = takeCommand().lower()
+            if 'wikipedia' in query:
+                speak("Searching Wikipedia...")
+                query = query.replace("wikipedia" , "")
+                results = wikipedia.summary(query, sentences=1)
+                print(results)
+                speak("According to wikipedia")
+                speak(results)
 
-        elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
+            elif 'open youtube' in query:
+                webbrowser.open("youtube.com")
 
-        elif 'open google' in query:
-            webbrowser.open("google.com")
+            elif 'open google' in query:
+                webbrowser.open("google.com")
 
-        elif 'open whatsapp' in query:
-            webbrowser.open("https://web.whatsapp.com/")
+            elif 'open whatsapp' in query:
+                webbrowser.open("https://web.whatsapp.com/")
 
-        elif 'play music' in query:
-            music_dir = 'D:\\Songs'
-            songs = os.listdir(music_dir)
-            print(songs)
-            os.startfile(os.path.join(music_dir, songs[0]))
+            elif 'play music' in query:
+                music_dir = 'D:\\Songs'
+                songs = os.listdir(music_dir)
+                print(songs)
+                os.startfile(os.path.join(music_dir, songs[0]))
 
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"Sir, The time is {strTime}\n")
+            elif 'the time' in query:
+                strTime = datetime.datetime.now().strftime("%H:%M:%S")
+                speak(f"Sir, The time is {strTime}\n")
 
-        elif 'open code' in query:
-            codePath = "C:\\Users\\Kamalpreet Singh\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath)
+            elif 'open code' in query:
+                codePath = "C:\\Users\\Kamalpreet Singh\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+                os.startfile(codePath)
 
-        elif 'open linkedin' in query:
-            webbrowser.open("https://www.linkedin.com/in/kamalpreet-singh-8558131a0/")
-            speak("Opening your linked account")
+            elif 'open linkedin' in query:
+                webbrowser.open("https://www.linkedin.com/in/kamalpreet-singh-8558131a0/")
+                speak("Opening your linked account")
 
-        elif 'open facebook' in query:
-            webbrowser.open("https://www.facebook.com/profile.php?id=100014358135907")
-            speak("Opening Facebook")
+            elif 'open facebook' in query:
+                webbrowser.open("https://www.facebook.com/profile.php?id=100014358135907")
+                speak("Opening Facebook")
 
-        elif 'open instagram' in query:
-            webbrowser.open("https://www.instagram.com/kamalpreet__singh__/")
-            speak("Opening Instagram")
+            elif 'open instagram' in query:
+                webbrowser.open("https://www.instagram.com/kamalpreet__singh__/")
+                speak("Opening Instagram")
 
-        elif 'how are you' in query:
-            speak("Sir I am Great")
+            elif 'how are you' in query:
+                speak("Sir I am Great")
 
-        elif 'your name' in query:
-            speak("Sir my name is Kamalexa")
+            elif 'your name' in query:
+                speak("Sir my name is Kamalexa")
+
+            elif 'search' in query:
+                query = query.replace("search" , "")
+                webbrowser.open(f"https://www.google.com/search?q=${query}")
+
+        except Exception as e:
+            print(e)
 
 
 
